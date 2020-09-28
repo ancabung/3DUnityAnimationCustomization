d304d839a6372f446dd65bea9c1ef16396c6d8dd
ancabung <ancabung@gmail.com>
2020-09-29 01:05:16 +0700

    Initial commit

---
+++ Assets/ButtonHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    Animator animator;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        
+    }
+    public void SetAnimation()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetBool("ActivateAnimation", true);
+        }
+    }
+}
